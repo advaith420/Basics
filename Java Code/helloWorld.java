32c1dfbea0b562dbdb32c1391ca603c580a74a85
advaith420 <18bcar6002@jainuniversity.ac.in>
2021-05-21 17:30:19 +0530

    Code

---
+++ Java Code/helloWorld.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class helloWorld
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int myInput = input.nextInt();
+        System.out.println("Hello World");
+    }
+}
